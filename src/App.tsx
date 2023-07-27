2df23f886b336a0dd8f251124f50de4ae5bb634f
Kristischik <kristinagi@mail.ru>
2023-07-27 23:52:55 +0300

    [step2] added Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from "./components/Button";
 import {ButtonTypes} from "./components/Button/Button";
+import Input from "./components/Input";
 
 
 const App = () => {
   return (
     <div><Button type={ButtonTypes.Primary} title={"Primary"} onClick={() => {}}/>
       <Button type={ButtonTypes.Secondary} title={"Secondary"} onClick={() => {}}/>
+      <Input title={"Title"} placeholder={'Placeholder'} onChange={() => {}} value={""}/>
      </div>
   );
 }
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,61 @@
+import React, { ChangeEvent, KeyboardEvent, FC } from "react";
+import classNames from "classnames";
+
+// import {useThemeContext} from "src/context/Theme";
+// import {Theme} from "src/@types";
+
+import styles from "./Input.module.scss";
+
+type InputProps = {
+  title: string;
+  placeholder: string;
+  onChange: (value: string) => void;
+  value: string;
+  disabled?: boolean;
+  errorText?: string;
+  className?: string;
+  onKeyDown?: (event: KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+};
+
+const Input: FC<InputProps> = ({
+                                 title,
+                                 errorText,
+                                 placeholder,
+                                 onChange,
+                                 disabled,
+                                 value,
+                                 onKeyDown
+                               }) => {
+
+  // const { themeValue } = useThemeContext();
+  const onInputChange = (
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    onChange(event.target.value);
+  };
+
+  const inputProps = {
+    onChange: onInputChange,
+    value,
+    placeholder,
+    className: classNames(styles.input, {
+      [styles.disabled]: disabled,
+      [styles.errorInput]: errorText,
+    }),
+    onKeyDown
+  };
+
+  return (
+    <div  className={classNames(styles.container,
+    //   {
+    //   [styles.darkContainer]: themeValue === Theme.Dark,
+    // }
+    )}>
+      <div className={styles.title}>{title}</div>
+      <input {...inputProps} />
+      {errorText && <div className={styles.errorText}>{errorText}</div>}
+    </div>
+  );
+};
+
+export default Input;
